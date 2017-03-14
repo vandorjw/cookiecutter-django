0e7d33e2d7f45fa6f04a3b422d7180a57190d311
Joost VanDorp <joostvandorp@gmail.com>
2017-03-14 16:18:17 -0400

    remove sites

--- {{cookiecutter.project_slug}}/config/settings.py
+++ {{cookiecutter.project_slug}}/config/settings.py
@@ -29,7 +29,6 @@ DJANGO_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
