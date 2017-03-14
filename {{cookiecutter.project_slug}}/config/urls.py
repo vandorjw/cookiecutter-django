533d947a3fd805c4b5d0ce04ece4051f2360f747
Joost VanDorp <joostvandorp@gmail.com>
2017-03-14 16:13:52 -0400

    fix broken url pattern

--- {{cookiecutter.project_slug}}/config/urls.py
+++ {{cookiecutter.project_slug}}/config/urls.py
@@ -19,7 +19,7 @@ urlpatterns = [
 urlpatterns += [
     url(r'^admin/', admin.site.urls),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^api/', include('api.urls', namespace='project_api')),
+    url(r'^api/', include('apps.api.urls', namespace='project_api')),
 ]
 
 
