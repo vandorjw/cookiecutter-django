ff60e31cb3b0f7963cc1641bbaaaddd5470aefb8
Joost VanDorp <joostvandorp@gmail.com>
2017-03-14 15:38:17 -0400

    admin url ended with '$'

--- {{cookiecutter.project_slug}}/config/urls.py
+++ {{cookiecutter.project_slug}}/config/urls.py
@@ -17,7 +17,7 @@ urlpatterns = [
 ]
 
 urlpatterns += [
-    url(r'^admin/$', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^api/', include('api.urls', namespace='project_api')),
 ]
